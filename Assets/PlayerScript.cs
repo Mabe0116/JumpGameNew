d7a808145533378d18b9e81a488ee707e7638a8b
Mabe0116 <115063548+Mabe0116@users.noreply.github.com>
2024-05-23 12:19:19 +0900

    完了

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,26 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        const float moveSpeed  = 1.0f;
+        const float moveJump = 10.0f; 
+
+        Vector3 v = rb.velocity;
+        if(UnityEngine.Input.GetKey(KeyCode.RightArrow))
+        {
+            v.x = moveSpeed;
+        }
+        else if(UnityEngine.Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x = -moveSpeed;
+        }
+        else
+        {
+            v.x = 0;
+        }
+        if(UnityEngine.Input.GetKey(KeyCode.Space))
+        {
+            v.y = moveJump;
+        }
+        rb.velocity = v;
     }
 }
